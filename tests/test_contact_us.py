0372b229addf0f5d29594118db5f0cb115c2a581
Irina <irinnaarzumanyan@gmail.com>
2026-09-19 17:26:44 +0400

    Fix intentionally introduced test failure

--- tests/test_contact_us.py
+++ tests/test_contact_us.py
@@ -3,8 +3,8 @@ from playwright.sync_api import expect
 from utils.logger import get_logger
 
 
-
 logger = get_logger(__name__)
+
 
 def test_contact_us_form(contact_us_page, test_data):  # fixture used
 
@@ -21,7 +21,8 @@ def test_contact_us_form(contact_us_page, test_data):  # fixture used
     contact_us_page.click_submit_button()
     # contact_us_page.page.wait_for_timeout(5000)
     logger.info("Verifying that contact us success message is visible")
-    expect(contact_us_page.contact_us_success_message).to_have_text(re.compile("Success! Your details have been submitted successfully."))
+    expect(contact_us_page.contact_us_success_message).to_have_text(
+        re.compile("Success! Your details have been submitted successfully."))
     contact_us_page.click_home_button()
     logger.info("Verifying that 'Home' link is visible")
     expect(contact_us_page.home_page_link_text).to_be_visible()
