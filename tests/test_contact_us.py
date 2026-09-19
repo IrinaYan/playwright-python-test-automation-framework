34e7457614fd9f72404fd3c44f58a76dffda2dbb
Irina <irinnaarzumanyan@gmail.com>
2026-09-19 17:08:26 +0400

    Test CI failure artifact handling

--- tests/test_contact_us.py
+++ tests/test_contact_us.py
@@ -21,7 +21,7 @@ def test_contact_us_form(contact_us_page, test_data):  # fixture used
     contact_us_page.click_submit_button()
     # contact_us_page.page.wait_for_timeout(5000)
     logger.info("Verifying that contact us success message is visible")
-    expect(contact_us_page.contact_us_success_message).to_have_text(re.compile("Success! Your details have been submitted successfully."))
+    expect(contact_us_page.contact_us_success_message).to_have_text(re.compile("Success! Your details have been submitted successfully.XXX"))
     contact_us_page.click_home_button()
     logger.info("Verifying that 'Home' link is visible")
     expect(contact_us_page.home_page_link_text).to_be_visible()
